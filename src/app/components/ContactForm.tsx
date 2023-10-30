03e833c2b24ee1120126738c23b4b3ddb5070dbf
Dominik Croci <dominikrcroci@gmail.com>
2023-10-30 13:21:58 -0700

    email js build error test

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 import FadeOnScroll from './FadeOnScroll';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 function ContactForm() {
   const fields = [
