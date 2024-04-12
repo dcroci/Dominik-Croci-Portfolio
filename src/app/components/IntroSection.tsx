6f8e3e898b10c4793d5b35ab63829a3ca205bdd0
Dominik Croci <dominikrcroci@gmail.com>
2024-04-11 17:27:28 -0700

    update headshot png

--- src/app/components/IntroSection.tsx
+++ src/app/components/IntroSection.tsx
@@ -8,20 +8,17 @@ function IntroSection() {
     >
       <div className="flex flex-col sm:flex-col items-center md:flex-row-reverse lg:flex-row-reverse xl:flex-row-reverse gap-8 intro">
         <Image
-          src={'/updated-headshot.jpg'}
+          src={'/headshot.png'}
           alt="Professional photo of me"
           width={'250'}
-          height={'50'}
-          className={`rounded-full mt-12 `}
+          height={'250'}
+          className={`rounded-full  `}
           priority={true}
         />
         <h1 className="text-7xl py-6 text-center">{`Hey, I'm Dominik`}</h1>
       </div>
       <p className="text-4xl py-6 text-center">
-        Developer of <span style={{ color: '#f39237' }}>responsive</span>,
-        <span style={{ color: '#00ffff' }}> accessible</span>, and
-        <span style={{ color: '#70e000' }}> engaging </span>
-        web applications
+        Developer of responsive, accessible, and engaging web applications
       </p>
     </section>
   );
